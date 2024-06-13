6f823ad047bc37809b0472d9488c990ee9071701
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-13 18:43:00 +0300

    1

---
+++ database/migrations/2024_06_13_154254_create__prog_binding_eg_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('_prog_binding_eg', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('_prog_binding_eg');
+    }
+};
