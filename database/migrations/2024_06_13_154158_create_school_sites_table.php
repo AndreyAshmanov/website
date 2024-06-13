df48086a3ed55f86c718702e0f7cd4ad85c3eebb
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-13 19:35:56 +0300

    8 Table

--- database/migrations/2024_06_13_154158_create_school_sites_table.php
+++ database/migrations/2024_06_13_154158_create_school_sites_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('school_sites', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('Название', 45);
+            $table->int('Привязка вуза к площадке_Id школы');
+            $table->int('Привязка вуза к площадке_Id площадки');
         });
     }
 
