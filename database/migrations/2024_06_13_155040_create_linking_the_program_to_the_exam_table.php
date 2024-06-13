163ad99f49e2340bc1fd6e1baad896345edd175d
Qkolo <132664736+Qkolo@users.noreply.github.com>
2024-06-13 19:31:39 +0300

    Update 2024_06_13_155040_create_linking_the_program_to_the_exam_table.php

--- database/migrations/2024_06_13_155040_create_linking_the_program_to_the_exam_table.php
+++ database/migrations/2024_06_13_155040_create_linking_the_program_to_the_exam_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('linking_the_program_to_the_exam', function (Blueprint $table) {
-            $table->int('Id программы');
+            $table->int('id программы');
             $table->int('id экзамена');
             $table->int('Образовательная программа вуза_Id');
         });
