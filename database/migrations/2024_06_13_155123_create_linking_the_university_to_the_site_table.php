4b8106de481752be528b468453af0cd095f0863f
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-13 20:52:50 +0300

    Привязка вуза к площадке

--- database/migrations/2024_06_13_155123_create_linking_the_university_to_the_site_table.php
+++ database/migrations/2024_06_13_155123_create_linking_the_university_to_the_site_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('linking_the_university_to_the_site', function (Blueprint $table) {
-            $table->int('id вуза');
-            $table->int('id площадки');
+            $table->integer('id вуза');
+            $table->integer('id площадки');
         });
     }
 
