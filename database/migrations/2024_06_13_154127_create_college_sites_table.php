bd34c8311aa2d80e436f4589bd49f049401e5877
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-13 19:32:02 +0300

    6 Table

--- database/migrations/2024_06_13_154127_create_college_sites_table.php
+++ database/migrations/2024_06_13_154127_create_college_sites_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('college_sites', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('Название', 45);
+            $table->int('Привязка вуза к площадке_Id вуза');
+            $table->int('Привязка вуза к площадке_Id площадки');
         });
     }
 
