3e036e563198c3800ead435ae79b9e9b816cc36c
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-13 19:39:07 +0300

    Table 9

--- database/migrations/2024_06_13_154254_create_prog_binding_eg_table.php
+++ database/migrations/2024_06_13_154254_create_prog_binding_eg_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('_prog_binding_eg', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('Id программы');
+            $table->increments('id направления');
         });
     }
 
