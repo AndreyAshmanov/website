8b5397017710337f599772ce4de6e487fd80ad53
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-15 02:41:06 +0300

    Площадки вузов

---
+++ database/migrations/2024_06_13_154128_create_university_sites_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('university_sites', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->integer('Linking_the_university_to_the_site_of_the_university');
+            $table->integer('Linking_the university_to_the_site_ID_of_the_site');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('university_sites');
+    }
+};
