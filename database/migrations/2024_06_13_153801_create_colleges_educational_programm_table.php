1dd40c25d2699b7314113d881e7fe170f4140ee3
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-15 02:16:30 +0300

    Образовательная программа колледжа

--- database/migrations/2024_06_13_153801_create_colleges_educational_programm_table.php
+++ database/migrations/2024_06_13_153801_create_colleges_educational_programm_table.php
@@ -12,16 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('colleges_educational_programm', function (Blueprint $table) {
-            $table->id();
+            $table->id()->index();
             $table->boolean('The_form_of_education');
             $table->boolean('Cost/Budget');
             $table->integer('Budget_places');
             $table->integer('Number_of_paid_seats');
             $table->integer('The_average_score_for_admission');
             $table->integer('College_sites_id');
-            $table->integer('Budget_place_Id1');
-            $table->integer('Training_FORM_ID');
-            $table->integer('College_SITES_ID1');
+            $table->integer('Budget_place_Id1')->index();
+            $table->integer('Training_FORM_ID')->index();
+            $table->integer('College_SITES_ID1')->index();
             $table->integer('LinkingProg/EG_ID_of_program')->index();
             $table->integer('BindingProg/EG_ID_direction')->index();
         });
