30a237303c347f4a308d1389f9c3dd3e20431f21
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-15 02:27:59 +0300

    Образовательная программа вуза

--- database/migrations/2024_06_13_153845_create_educational_programm_of_the_university_table.php
+++ database/migrations/2024_06_13_153845_create_educational_programm_of_the_university_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->integer('Budget_places');
             $table->integer('Number_of_paid_seats');
             $table->integer('Budget_place_Id')->index();
-            $table->integer('LinkingProg/egIDprogram')->index();
+            $table->integer('LinkingProg/egIDprog')->index();
             $table->integer('BindingProg/egIDdirection')->index();
             $table->integer('LinkingProg/EX_ID_program')->index();
             $table->integer('LinkingProg/EX_ID_exam')->index();
