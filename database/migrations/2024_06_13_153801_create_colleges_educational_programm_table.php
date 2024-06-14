75e07825a422836347e69ad6297b21baf659dd23
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-15 02:31:23 +0300

    Образовательная программа колледжа

--- database/migrations/2024_06_13_153801_create_colleges_educational_programm_table.php
+++ database/migrations/2024_06_13_153801_create_colleges_educational_programm_table.php
@@ -22,8 +22,8 @@ return new class extends Migration
             $table->integer('Budget_place_Id1')->index();
             $table->integer('Training_FORM_ID')->index();
             $table->integer('College_SITES_ID1')->index();
-            $table->integer('LinkingProg/egIDprog')->index();
-            $table->integer('BindingProg/egIDdirection')->index();
+            $table->integer('Link_Prog_eg_id_programm')->index();
+            $table->integer('Binding_Prog_eg_id_direction')->index();
         });
     }
 
