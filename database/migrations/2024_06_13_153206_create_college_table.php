4a41b06efb1af43f1dfa100243f4f7e50708f7a6
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-15 01:14:56 +0300

    Колледж

--- database/migrations/2024_06_13_153206_create_college_table.php
+++ database/migrations/2024_06_13_153206_create_college_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('college', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary();;
             $table->string('name', 45);
             $table->string('Address', 45);
             $table->integer('Rating');
