1c3ec59ad84c5b5234eae169e12f3bca83df423f
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-15 12:26:24 +0300

    Вуз

---
+++ database/migrations/2024_06_13_155419_create_university_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('university', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->string('Address', 45);
+            $table->integer('Rating');
+            $table->string('Description', 150);
+            $table->integer('Linking_the_university_to_the_site_of_the_university');
+            $table->integer('Linking_the_university_to_the_site_ID_of_the_site');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('university');
+    }
+};
