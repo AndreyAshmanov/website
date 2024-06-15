b40b374f1880d1e6e03fd0eb026325efe45cbd32
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-15 12:39:07 +0300

    Экзамен

---
+++ database/migrations/2024_06_13_155535_create_exam_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exam', function (Blueprint $table) {
+            $table->id()->index();
+            $table->string('Subject', 45);
+            $table->integer('Passing_score');
+            $table->integer('Minimum_score');
+            $table->boolean('Is_it_possible_to_take_an_internal_exam')->default(true);
+            $table->integer('Linking_program_exam_ID_program')->index();
+            $table->integer('Linking_program_exam_ID_exam')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exam');
+    }
+};
