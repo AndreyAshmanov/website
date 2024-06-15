97a9f2b60d104ed5d706f68331f61623e9ed4007
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-15 12:03:39 +0300

    Привязка программы к направлению

---
+++ database/migrations/2024_06_13_155016_create_linking_the_program_to_the_direction_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('linking_the_program_to_the_direction', function (Blueprint $table) {
+            $table->integer('Program_ID')->primary();
+            $table->integer('direction_ID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('linking_the_program_to_the_direction');
+    }
+};
