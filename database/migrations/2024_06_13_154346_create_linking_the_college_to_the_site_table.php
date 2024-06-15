9cef45fafab17dc9215331317b6ef0f767bbc615
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-15 11:56:20 +0300

    Привязка колледжа к площадке

---
+++ database/migrations/2024_06_13_154346_create_linking_the_college_to_the_site_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('linking_the_college_to_the_site', function (Blueprint $table) {
+            $table->integer('college_ID')->primary();
+            $table->integer('site_ID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('linking_the_college_to_the_site');
+    }
+};
