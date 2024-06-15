03b1c757e17889f819549333adb6a8cc1920bea4
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-15 12:28:34 +0300

    Вуз

--- database/migrations/2024_06_13_155419_create_university_table.php
+++ database/migrations/2024_06_13_155419_create_university_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('university', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 45);
-            $table->string('Address', 45);
-            $table->integer('Rating');
-            $table->string('Description', 150);
-            $table->integer('Linking_the_university_to_the_site_of_the_university');
-            $table->integer('Linking_the_university_to_the_site_ID_of_the_site');
+            $table->id()->index();
+            $table->string('name', 45)->index();
+            $table->string('Address', 45)->index();
+            $table->integer('Rating')->index();
+            $table->string('Description', 150)->index();
+            $table->integer('Linking_university_site_ID_university')->index();
+            $table->integer('Linking_university_site_ID_site')->index();
         });
     }
 
