a27c00c1d7d1174334c4b622c7ebba85e3ea40b8
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-15 12:32:58 +0300

    Привязка образовательной программы к направлению обучения

--- database/migrations/2024_06_13_155227_create_linking_the_educational_program_to_the_field_of_study_table.php
+++ database/migrations/2024_06_13_155227_create_linking_the_educational_program_to_the_field_of_study_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('linking_the_educational_program_to_the_field_of_study', function (Blueprint $table) {
+        Schema::create('linking_the_educational_program_field_study', function (Blueprint $table) {
             $table->integer('Program_ID')->primary();
             $table->integer('direction_ID')->index();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('linking_the_educational_program_to_the_field_of_study');
+        Schema::dropIfExists('linking_the_educational_program_field_study');
     }
 };
