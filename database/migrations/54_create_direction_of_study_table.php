d566d5b4b7dcda910e5a492561a01595aa90a7bf
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-17 14:34:44 +0300

    Направление обучения

---
+++ database/migrations/54_create_direction_of_study_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('direction_of_study', function (Blueprint $table) {
+            $table->id()->index();
+            $table->string('name', 45);
+            $table->integer('Duration of study (years)');
+            $table->unsignedBigInteger('LinkingProg/EG_ID_program')->index();
+            $table->unsignedBigInteger('BindingProg/EG_ID_direction')->index();
+            $table->primary(['id', 'LinkingProg/EG_ID_program', 'BindingProg/EG_ID_direction']);
+            $table->foreign('LinkingProg/EG_ID_program')->references('Program_ID')->on('linking_the_program_to_the_direction');
+            $table->foreign('BindingProg/EG_ID_direction')->references('direction_ID')->on('linking_the_program_to_the_direction');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('direction_of_study');
+    }
+};
