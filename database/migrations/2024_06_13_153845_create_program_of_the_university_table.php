d1a4dbe3a011c71fd7a535f78033b479ee4b48ca
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-17 12:18:36 +0300

    Образовательная программа вуза

---
+++ database/migrations/2024_06_13_153845_create_program_of_the_university_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('program_of_the_university', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('The_form_of_education')->default(true);
+            $table->boolean('Cost/Budget')->default(true);
+            $table->integer('Budget_places');
+            $table->integer('Number_of_paid_seats');
+            $table->integer('Budget_place_Id')->index();
+            $table->integer('Binding_Prog_eg_id_programm')->index();
+            $table->integer('Binding_Prog_eg_id_direction')->index();
+            $table->integer('LinkingProg/EX_ID_program')->index();
+            $table->integer('LinkingProg/EX_ID_exam')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('program_of_the_university');
+    }
+};
