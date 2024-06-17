6cd0f80b655b73bc40e322be99de1218e6aadc56
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-17 13:38:07 +0300

    Форма обучения

---
+++ database/migrations/44_create_the_form_of_education_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('the_form_of_education', function (Blueprint $table) {
+            $table->id();
+            $table->integer('In_person');
+            $table->integer('In_absentia');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('the_form_of_education');
+    }
+};
