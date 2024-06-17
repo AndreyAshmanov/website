9c297dfac2db0082946fd857e29413d2fa6dbe4d
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-17 17:59:47 +0300

    Площадки школ

--- database/migrations/2024_06_13_154158_create_school_sites_table.php
+++ database/migrations/2024_06_13_154158_create_school_sites_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('school_sites', function (Blueprint $table) {
             $table->id()->index();
             $table->string('name', 45);
-            $table->integer('Linking_university_school_ID_site')->index();
-            $table->integer('Linking_university_site_ID_site')->index();
+            $table->integer('Linking_school_school_ID_site')->index();
+            $table->integer('Linking_school_site_ID_site')->index();
         });
     }
 
