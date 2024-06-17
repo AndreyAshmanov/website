de96d88eecea627bd20f28d0ad1f3c03a85fb21f
AndreyAshmanov <andreyashmanov42@gmail.com>
2024-06-17 14:44:31 +0300

    Привязка программы к направлению

--- database/migrations/45_create_linking_the_program_to_the_direction_table.php
+++ database/migrations/45_create_linking_the_program_to_the_direction_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('linking_the_program_to_the_direction', function (Blueprint $table) {
-            $table->unsignedBigInteger('Program_ID');
-            $table->unsignedBigInteger('direction_ID');
+            $table->unsignedBigInteger('Program_ID')->index();
+            $table->unsignedBigInteger('direction_ID')->index();
             $table->primary(['Program_ID', 'direction_ID']);
         });
     }
